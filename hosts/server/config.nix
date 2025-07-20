14de4524539b6dbef97236770c0bcedc3cdc6d9f
Stef-00012 <me@stefdp.com>
2025-07-20 20:08:37 +0200

    enable personal api & update your_spotify token

--- hosts/server/config.nix
+++ hosts/server/config.nix
@@ -267,7 +267,7 @@
             };
 
             api = {
-                enable = false;
+                enable = true;
                 domain = "api.stefdp.com";
                 port = 3019;
                 repoUrl = "https://github.com/Stef-00012/api";
