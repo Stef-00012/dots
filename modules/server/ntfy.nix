a2b5cb0c39750191a82e92360ab0c396035bebbf
Stef-00012 <me@stefdp.com>
2025-07-22 11:27:23 +0200

    fix error

--- modules/server/ntfy.nix
+++ modules/server/ntfy.nix
@@ -117,9 +117,7 @@ in
         # NTFY_WEB_PUSH_PRIVATE_KEY=xxx
         modules.common.sops.secrets.ntfy-env.path = "/var/secrets/ntfy-env";
 
-        systemd.services.ntfy-sh.environmentFiles = [
-            "/var/secrets/ntfy-env"
-        ];
+        systemd.services.ntfy-sh.serviceConfig.EnvironmentFile = "/var/secrets/ntfy-env";
 
         services.ntfy-sh = {
             enable = true;
